46aa9ec2211ded92b4f688628a688da24f124d19
Joe Berthels <joe2157b@gmail.com>
2024-03-03 12:44:44 +0000

    Changed ISA to a more reasonable target

--- functionalUnit.sv
+++ functionalUnit.sv
@@ -5,6 +5,9 @@ localparam EXECUTE_STAGE = 1;
 localparam MEMORY_STAGE = 2;
 localparam WRITEBACK_STAGE = 3;
 
+localparam MEMORY_FORMAT = 0;
+localparam IALU_FORMAT = 1;
+
 module functionalUnit #(
 	FUID
 ) (
@@ -18,54 +21,38 @@ module functionalUnit #(
 	output logic doneBatches
 );
 
-	logic [INSTRUCTION_WIDTH-1:0] pipelinedInstructions [BATCH_INSTRUCTION_WIDTH-1:0] [PIPELINE_STAGES-1:0];
-	logic [FORMAT_WIDTH-1:0] pipelinedFormats [BATCH_INSTRUCTION_WIDTH-1:0] [PIPELINE_STAGES-1:0];
+	logic [INSTRUCTION_WIDTH-1:0] instructionPipeline [PIPELINE_STAGES-1:0];
+	logic [FORMAT_WIDTH-1:0] formatPipeline [PIPELINE_STAGES-1:0];
 	logic endTagPipeline [PIPELINE_STAGES-1:0];
 
 
 	always_ff @( posedge clock ) begin
 		if (reset) begin
+			doneBatches <= 0;
+			instructionPipeline[0] <= 0;
+			formatPipeline[0] <= 0;
 			endTagPipeline[0] <= 0;
 		end else begin
+			instructionPipeline[0] <= batch[METADATA_WIDTH+:INSTRUCTION_WIDTH];
+			formatPipeline[0] <= batch[1+:FORMAT_WIDTH];
 			endTagPipeline[0] <= endTag;
-		end
-	end
-
-	always_ff @( posedge clock ) begin
-		if (reset) begin
-			doneBatches <= 0;
-		end else begin
 			doneBatches <= endTagPipeline[PIPELINE_STAGES-1];
 		end
 	end
 
 	generate
-		genvar instrId;
 		genvar pipelineStage;
 
-		for (instrId = 0; instrId < BATCH_INSTRUCTION_WIDTH ; instrId++ ) begin
-
+		for (pipelineStage = 1; pipelineStage < PIPELINE_STAGES ; pipelineStage++ ) begin
 			always_ff @( posedge clock ) begin
 				if (reset) begin
-					pipelinedInstructions[instrId][0] <= 0;
-					pipelinedFormats[instrId][0] <= 0;
-				end else begin 
-					pipelinedInstructions[instrId][0] <= batch[METADATA_WIDTH+(instrId*INSTRUCTION_WIDTH)+:INSTRUCTION_WIDTH];
-					pipelinedFormats[instrId][0] <= batch[1+(instrId*FORMAT_WIDTH)+:FORMAT_WIDTH];
-				end
-			end
-
-			for (pipelineStage = 1; pipelineStage < PIPELINE_STAGES ; pipelineStage++ ) begin
-				always_ff @( posedge clock ) begin
-					if (reset) begin
-						pipelinedInstructions[instrId][pipelineStage] <= 0;
-						pipelinedFormats[instrId][pipelineStage] <= 0;
-						endTagPipeline[pipelineStage] <= 0;
-					end else begin
-						pipelinedInstructions[instrId][pipelineStage] <= pipelinedInstructions[instrId][pipelineStage-1];
-						pipelinedFormats[instrId][pipelineStage] <= pipelinedFormats[instrId][pipelineStage-1];
-						endTagPipeline[pipelineStage] <= endTagPipeline[pipelineStage-1];
-					end
+					instructionPipeline[pipelineStage] <= 0;
+					formatPipeline[pipelineStage] <= 0;
+					endTagPipeline[pipelineStage] <= 0;
+				end else begin
+					instructionPipeline[pipelineStage] <= instructionPipeline[pipelineStage-1];
+					formatPipeline[pipelineStage] <= formatPipeline[pipelineStage-1];
+					endTagPipeline[pipelineStage] <= endTagPipeline[pipelineStage-1];
 				end
 			end
 		end
--- vlsiwCore.sv
+++ vlsiwCore.sv
@@ -1,11 +1,8 @@
-
-localparam BATCH_INSTRUCTION_WIDTH = 5;
 localparam INSTRUCTION_WIDTH = 44;
-localparam TEMPLATE_WIDTH = FORMAT_WIDTH * BATCH_INSTRUCTION_WIDTH;
-localparam METADATA_WIDTH = TEMPLATE_WIDTH + 1 + /* Reserved */ 20;
-localparam BATCH_WIDTH = BATCH_INSTRUCTION_WIDTH * INSTRUCTION_WIDTH + METADATA_WIDTH;
-localparam BATCH_WIDTH_BYTES = BATCH_WIDTH / 8;
 localparam FORMAT_WIDTH = 3;
+localparam METADATA_WIDTH = FORMAT_WIDTH + 1 + /* Reserved */ 16;
+localparam BATCH_WIDTH = INSTRUCTION_WIDTH + METADATA_WIDTH;
+localparam BATCH_WIDTH_BYTES = BATCH_WIDTH / 8;
 
 localparam ADDRESS_WIDTH = 64;
 
@@ -14,21 +11,22 @@ localparam ADDRESS_WIDTH = 64;
 module vlsiwCore #(
 	parameter FUNCTIONAL_UNITS = 8,
 	
-	localparam FETCH_WIDTH = FUNCTIONAL_UNITS * BATCH_WIDTH
+	localparam FETCH_WIDTH = FUNCTIONAL_UNITS * BATCH_WIDTH,
+	localparam FETCH_WIDTH_BYTES = FETCH_WIDTH / 8
 ) (
 	input logic clock,
 	input logic reset
 );
 	/* Temporary, will be replaced by actual memory */
-	logic [(FETCH_WIDTH*2)-1:0] batch_memory;
+	logic [(FETCH_WIDTH)-1:0] batch_memory;
 	initial begin
 		batch_memory[0] = 1'h0;
-		batch_memory[35:1] = 35'h0;
-		batch_memory[255:36] = 220'h55555555555_44444444444_33333333333_22222222222_11111111111;
+		batch_memory[19:1] = 19'h0;
+		batch_memory[63:20] = 44'h11111111111;
 
-		batch_memory[256] = 1'h1;
-		batch_memory[256+35:256+1] = 35'h0;
-		batch_memory[256+255:256+36] = 220'hAAAAAAAAAAA_99999999999_88888888888_77777777777_66666666666;
+		batch_memory[64+0] = 1'h1;
+		batch_memory[64+19:64+1] = 19'h0;
+		batch_memory[64+63:64+20] = 44'h22222222222;
 	end
 
 
@@ -37,6 +35,7 @@ module vlsiwCore #(
 	logic [FETCH_WIDTH-1:0] fetchBuffer;
 	logic [FUNCTIONAL_UNITS-1:0] stopDaisyChain;
 	logic [$clog2(FUNCTIONAL_UNITS)-1:0] nextBatch;
+	logic stopBatches;
 	logic doneFetch, haltFetch, endBatches, doneBatches, doingFetch;
 
 	always_ff @( posedge clock ) begin
@@ -45,6 +44,7 @@ module vlsiwCore #(
 			pc <= 0;
 			haltFetch <= 0;
 			doneFetch <= 0;
+			doingFetch <= 1;
 		end else if (doneBatches) begin 
 			haltFetch <= 0;
 			doingFetch <= 1;
@@ -54,10 +54,13 @@ module vlsiwCore #(
 		end else if (doneFetch) begin
 			if (nextBatch != 0) begin
 				pc <= pc + (nextBatch * BATCH_WIDTH_BYTES);
+			end else begin 
+				pc <= pc + FETCH_WIDTH_BYTES;
+			end
+			if (stopBatches) begin
+
 				haltFetch <= 1;
 				endBatches <= 1;
-			end else begin 
-				pc <= pc + BATCH_WIDTH_BYTES;
 			end
 			doneFetch <= 0;
 		end else begin
@@ -76,9 +79,19 @@ module vlsiwCore #(
 					stopDaisyChain = 0;
 					nextBatch = 0;
 				end else if (stopDaisyChain[batchIndex] == 0) begin
-					stopDaisyChain[batchIndex+1] = fetchBuffer[batchIndex*BATCH_WIDTH+:BATCH_WIDTH][0];
-				end else begin
-					nextBatch = batchIndex;
+					if (batchIndex != FUNCTIONAL_UNITS-1) begin
+						if (fetchBuffer[batchIndex*BATCH_WIDTH+:BATCH_WIDTH][0]) begin
+							stopDaisyChain[batchIndex+1] = 1;
+							nextBatch = batchIndex+1;
+							stopBatches = 1;
+						end else begin
+							stopDaisyChain[batchIndex+1] = 0;
+						end
+					end else begin
+						if (fetchBuffer[batchIndex*BATCH_WIDTH+:BATCH_WIDTH][0]) begin
+							stopBatches = 1;
+						end
+					end
 				end
 			end
 
